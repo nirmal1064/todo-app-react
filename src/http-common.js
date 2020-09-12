a0ac965d1164392ea7fbbcb9cb170291d980f342
Nirmal Kumar <nirmal1064@gmail.com>
2020-09-12 17:24:10 +0530

    changed baseURL for production

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const development = process.env.NODE_ENV !== 'production'
 const http = axios.create({
-    baseURL: development? "http://localhost:8082/": "https://herokuapi",
+    baseURL: development? "http://localhost:8082/": "https://todoapi-mern.herokuapp.com/",
     headers: {
         "Content-type": "application/json"
     }
